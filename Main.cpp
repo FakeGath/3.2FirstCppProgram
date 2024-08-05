f4f4d382a0026d3bf4acd3eec9e4ea6e506cc72d
FakeGath <givanalvaradoath@gmail.com>
2024-08-05 17:32:37 -0600

    First Commit

---
+++ Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+consteval int get_value(){
+    return 3;
+}
+
+int main(){
+    
+    for (int i = 0; i < 10 ; i++)
+    {
+        std :: cout << "My name is Ivan" << std :: endl;
+    }
+    
+    return 0;
+
+
+}
